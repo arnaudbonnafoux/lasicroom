e70b13a6dd17be7c7379c1f0fc02783e2bd791a4
arnaudbonnafoux <bonnafouxarnaud@gmail.com>
2025-07-10 11:59:10 +0200

    update_gitignore

---
+++ lasicroom_back/lasicroom2.session.sql
@@ -0,0 +1,12 @@
+SELECT * FROM utilisateur;
+SELECT * FROM artiste;
+SELECT * FROM concert;
+SELECT * FROM reservation;
+SELECT * FROM accompagnement
+/*
+DELETE FROM artiste;
+SELECT * FROM artiste;
+WHERE id_artiste IN (2, 3, 4);
+*/
+
+
