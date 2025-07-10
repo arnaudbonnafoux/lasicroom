bab354ac9b628df203a43712a274e41172ea493b
arnaudbonnafoux <bonnafouxarnaud@gmail.com>
2025-07-10 19:08:35 +0200

    ajout_iframe

--- lasicroom_front/src/pages/accueil.js
+++ lasicroom_front/src/pages/accueil.js
@@ -14,40 +14,58 @@ const Accueil = () => {
       <main>
         <h1>Bienvenue à la sicRoom !</h1>
 
-          <div className='div_accueil'>
-            <div>
-              <img src="/images/photo_2.jpg" alt="Concert" />
-            </div>
-
-            <div className='texte'>
-              <h2 style={{textAlign:'center'}}>La sic Room !?</h2>
-
-              <p style={{padding:'12px'}}>
-                La sicRoom est une Scène de Musiques Actuelles (SMAC) dédiée à la création, la diffusion et le
-                partage des musiques d’aujourd’hui, dans toute leur diversité. Rock, pop, rap, électro, métal,
-                chanson, et bien plus encore : notre programmation reflète la richesse et l’énergie de la scène
-                musicale contemporaine.
-              </p>
-            </div>
-
-            <div className='info'>
-              <h2 style={{textAlign:'center'}}>Infos !</h2>
-              <ul>
-                <li><strong>Prochain concert : le Samedi 21 mai avec Titi et son orchestre !</strong></li>
-                <li><strong>Le concert de Kamélia est annulé.</strong></li>
-                <li><strong>Inline Block trio sera en résidence le jeudi 17 juin.</strong></li>
-              </ul>
-            </div>
+        <div className='div_accueil'>
+          <div>
+            <img src="/images/photo_2.jpg" alt="Concert" />
           </div>
 
+          <div className='texte'>
+            <h2 style={{ textAlign: 'center' }}>La sic Room !?</h2>
+
+            <p style={{ padding: '12px' }}>
+              La sicRoom est une Scène de Musiques Actuelles (SMAC) dédiée à la création, la diffusion et le
+              partage des musiques d’aujourd’hui, dans toute leur diversité. Rock, pop, rap, électro, métal,
+              chanson, et bien plus encore : notre programmation reflète la richesse et l’énergie de la scène
+              musicale contemporaine.
+            </p>
+          </div>
+
+          <div className='info'>
+            <h2 style={{ textAlign: 'center' }}>Infos !</h2>
+            <ul>
+              <li><strong>Prochain concert : le Samedi 21 mai avec Titi et son orchestre !</strong></li>
+              <li><strong>Le concert de Kamélia est annulé.</strong></li>
+              <li><strong>Inline Block trio sera en résidence le jeudi 17 juin.</strong></li>
+            </ul>
+          </div>
+        </div>
+
         <div className='div_live_streaming'>
-          <div style={{display:'flex', alignItems:'center'}}>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
             <h2>
               Le direct live des balances <span>→</span>
             </h2>
           </div>
           <div>
-            <img src="/images/photo_1.jpg" alt="Direct" />
+            <iframe
+              style={{
+                marginTop: '10px',
+                marginBottom: '10px',
+                /*borderRadius: '4px',*/
+                boxShadow: '0 8px 16px rgba(0, 0, 0, 0.75)',
+                borderRadius: '12px'
+              }}
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/YeHvUcdZ9t8?si=wO0bi7QKvHXASxtv"
+              title="YouTube video player"
+              Border="none"
+              allow="accelerometer; autoplay; clipboard-write; 
+              encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+
+            {/*<img src="/images/photo_1.jpg" alt="Direct" />*/}
           </div>
         </div>
       </main>
